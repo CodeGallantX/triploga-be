2933f7987764e5ba2c3ac371f6c0e5eeca656eeb
John A. Samuel <johnayobami77@proton.me>
2025-02-27 22:24:51 +0100

    fixed typo error in cors configuration

--- trip_logger/settings.py
+++ trip_logger/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'trip_logger.urls'
 
