36d2e4462edb894d62d958053d888ab837f9656a
John A. Samuel <johnayobami77@proton.me>
2025-02-28 03:49:21 +0100

    Updated ALLOWED HOSTS

--- trip_logger/settings.py
+++ trip_logger/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://triploga.vercel.app", "https://triploga-be.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["triploga.vercel.app", "triploga-be.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
